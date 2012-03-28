55c17365dc0a1323306d544178208a378dba6ab9
Almer Almazan <almeralmazan@gmail.com>
2012-03-29 01:18:10 +0800

    my base ci first commit

---
+++ application/views/includes/header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $title; ?></title>
+	<?php echo link_tag('assets/css/style.css'); ?>
+	<script src="<?php echo base_url('assets/js/libs/jquery-1.7.1.min.js'); ?>"></script>
+	<script src="<?php echo base_url('assets/js/libs/modernizr-2.5.3.min.js'); ?>"></script>
+	<script src="<?php echo base_url('assets/js/script.js'); ?>"></script>
+</head>
+<body>
